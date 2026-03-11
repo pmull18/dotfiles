5bc1caa2755dc7af569573bbbb8097659cfe42ce
pgmullen@umich.edu <104961081+pmull18@users.noreply.github.com>
2026-03-11 10:57:26 -0400

    fixed missing comma in wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,7 +6,7 @@ return{
 	default_prog = { "wsl.exe", "--exec", "zsh", "-l" },
 	default_domain = "WSL:Ubuntu",
 
-	font_size = 12.0
+	font_size = 12.0,
 	color_scheme = "Catppuccin Mocha",
 	font = wezterm.font("JetBrainsMono Nerd Font Mono"),
 }
