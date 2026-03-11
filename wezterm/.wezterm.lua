0a22ec68400406495f49e68d6d98934067054318
pgmullen@umich.edu <104961081+pmull18@users.noreply.github.com>
2026-03-11 19:09:48 -0400

    fixed wezterm lua file to be cross platform (windows and mac)

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,12 +1,17 @@
 local wezterm = require "wezterm"
 
-return{
-	window_decorations = "INTEGRATED_BUTTONS|RESIZE",
+local config = {}
 
-	default_prog = { "wsl.exe", "--exec", "zsh", "-l" },
-	default_domain = "WSL:Ubuntu",
+config.font_size = 12.0,
+config.color_scheme = "Catppuccin Mocha",
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono"),
+	
+if wezterm.target_triple:find("windows") then
+	config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+	config.default_prog = { "wsl.exe", "--exec", "zsh", "l" }
+	config.default_domain = "WSL:Ubuntu"
+else
+	config.window_decorations = "RESIZE"
+end
 
-	font_size = 12.0,
-	color_scheme = "Catppuccin Mocha",
-	font = wezterm.font("JetBrainsMono Nerd Font Mono"),
-}
+return config
