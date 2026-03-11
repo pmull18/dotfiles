06dd90062be007989617176c049385b7e473b46f
pgmullen@umich.edu <104961081+pmull18@users.noreply.github.com>
2026-03-11 19:12:56 -0400

    actually fixed wezterm lua this time

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,16 +2,15 @@ local wezterm = require "wezterm"
 
 local config = {}
 
-config.font_size = 12.0,
-config.color_scheme = "Catppuccin Mocha",
-config.font = wezterm.font("JetBrainsMono Nerd Font Mono"),
+config.font_size = 12.0
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 	
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+
 if wezterm.target_triple:find("windows") then
-	config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 	config.default_prog = { "wsl.exe", "--exec", "zsh", "l" }
 	config.default_domain = "WSL:Ubuntu"
-else
-	config.window_decorations = "RESIZE"
 end
 
 return config
