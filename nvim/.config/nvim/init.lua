3149751dfe47bcda75ed627ca5faaeda80d55b5d
pgmullen@umich.edu <104961081+pmull18@users.noreply.github.com>
2026-06-17 08:54:17 -0400

    forgot to add files

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,6 +3,7 @@ vim.o.number = true
 vim.o.expandtab = true
 vim.o.shiftwidth = 2
 vim.o.tabstop = 2
+vim.o.termguicolors = true
 
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { noremap = true })
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { noremap = true })
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -8,6 +8,10 @@ return {
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+      require('lspconfig.ui.windows').default_options = {
+        border = "rounded"
+      }
+
       vim.lsp.config("clangd", {
         capabilities = capabilities,
       })
